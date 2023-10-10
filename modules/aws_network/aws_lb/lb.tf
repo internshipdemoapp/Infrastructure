687ab2388d2367f0352de57eef2fcf61ead62edf
Ruslan <yarema2003r@gmail.com>
2023-10-10 16:53:33 +0300

    network/load balancer added

--- modules/aws_network/aws_lb/lb.tf
+++ modules/aws_network/aws_lb/lb.tf
@@ -32,3 +32,31 @@ resource "aws_alb_listener" "alb_default_listener_https" {
   }
   depends_on = [aws_acm_certificate.alb_certificate]
 }
+
+resource "aws_lb_listener_certificate" "alb_default_listener_certificate_https" {
+  depends_on = [aws_acm_certificate_validation.cert_validate]
+
+  listener_arn    = aws_alb_listener.alb_default_listener_https.arn
+  certificate_arn = "${aws_acm_certificate.alb_certificate.arn}"
+}
+
+resource "aws_alb_target_group" "service_target_group" {
+  name                 = "${var.namespace}-target-group-${var.environment}"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 5
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 60
+    matcher             = var.healthcheck_matcher
+    path                = var.healthcheck_endpoint
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 30
+  }
+
+  depends_on = [aws_alb.alb]
+}
