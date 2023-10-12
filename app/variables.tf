c7b8819012130d9162b753d31fa8cfe2fa2ee170
Anna Zalevska <annazalevzka@gmail.com>
2023-10-12 11:58:07 +0300

    Revert "Adding the AWS _ECS module"
    This reverts commit a1cd2366f3f4cf28d98c4ac89205695a527d4232.

--- app/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "namespace" {
-  type = string
-  default = "dev"
-}
--- modules/aws_ecs/aws_cluster/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecs_cluster" "default" {
-  name = "${var.namespace}-ecs-cluster-${var.environment}"
-
-  tags = {
-    Name     = "${var.namespace}-ecs-cluster-${var.environment}"
-  }
-}
