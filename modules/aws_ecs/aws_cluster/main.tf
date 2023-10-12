953bd7a74ab341cd8432bdd23ee7a5e5596db380
Anna Zalevska <annazalevzka@gmail.com>
2023-10-12 12:31:06 +0300

    Updated aws_task_definition

---
+++ modules/aws_ecs/aws_cluster/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecs_cluster" "default" {
+  name = "${var.namespace}-ecs-cluster-${var.environment}"
+
+  tags = {
+    Name     = "${var.namespace}-ecs-cluster-${var.environment}"
+  }
+}
+
