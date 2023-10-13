bc70e2dbcffd90e402ae19b72453e35640f42629
internshipdemoapp <147274235+internshipdemoapp@users.noreply.github.com>
2023-10-13 14:31:31 +0300

    Update modules/aws_ecs/aws_task_definition/main.tf file

--- modules/aws_ecs/aws_task_definition/main.tf
+++ modules/aws_ecs/aws_task_definition/main.tf
@@ -1,13 +1,22 @@
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = var.secret_name
+}
+
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
 resource "aws_ecs_task_definition" "default" {
-  family             = "${var.namespace}-ecs_task-definition-${var.environment}"
+  family             = "${var.namespace}-ecs-task-definition-${var.environment}"
   execution_role_arn = var.ecs_task_execution_role_arn
   task_role_arn      = var.ecs_task_iam_role_arn
 
   container_definitions = jsonencode([
     {
-
       name      = var.service_name
-      image     = "test"
+      image     = "494250472911.dkr.ecr.us-east-1.amazonaws.com/store-web-application:latest"
       cpu       = var.cpu_units
       memory    = var.memory
       essential = true
@@ -20,27 +29,19 @@ resource "aws_ecs_task_definition" "default" {
       ]
       environment : [
         {
-          name : "DBNAME",
+          name : "DbName",
           value : local.db_creds.DbName
         },
         {
-          name : "DBUSER",
+          name : "DbUsername",
           value : local.db_creds.DbUsername
         },
         {
-          name : "DBHOST",
-          value : aws_db_instance.rds_db.endpoint
+          name : "DbHost",
+          value : var.db_host
         },
         {
-          name : "DBSSLMODE",
-          value : "none"
-        },
-        {
-          name : "DBPORT",
-          value : 3306
-        },
-        {
-          name : "DBPASS",
+          name : "DbPassword",
           value : local.db_creds.DbPassword
         }
       ]
