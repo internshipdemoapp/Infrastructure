850cb848532a646c9982257fd034117bc2093fe4
internshipdemoapp <147274235+internshipdemoapp@users.noreply.github.com>
2023-10-13 14:43:24 +0300

    Update Update modules/aws_network/aws_sg/outputs.tf

--- modules/aws_network/aws_sg/outputs.tf
+++ modules/aws_network/aws_sg/outputs.tf
@@ -3,11 +3,11 @@ output "security_group_ec2_id" {
 }
 
 output "security_group_alb_id" {
-  value = aws_security_group.ec2.id
+  value = aws_security_group.alb.id
 }
 
 output "security_group_db_id" {
-  value = aws_security_group.ec2.id
+  value = aws_security_group.db.id
 }
 
 output "security_group_ec2" {
@@ -15,11 +15,11 @@ output "security_group_ec2" {
 }
 
 output "security_group_alb" {
-  value = aws_security_group.ec2
+  value = aws_security_group.alb
 }
 
 output "security_group_db" {
-  value = aws_security_group.ec2
+  value = aws_security_group.db
 }
 
 output "security_group_bastion_host_id" {
