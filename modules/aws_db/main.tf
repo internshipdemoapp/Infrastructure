7dc155748c873eefd5036b857758a9f54bb34e3a
internshipdemoapp <147274235+internshipdemoapp@users.noreply.github.com>
2023-10-13 14:01:49 +0300

    Update aws_db module

--- modules/aws_db/main.tf
+++ modules/aws_db/main.tf
@@ -8,29 +8,6 @@ locals {
   )
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-resource "aws_security_group" "rds_sg" {
-  name = "rds_sg"
-  description = "RDS Security Group"
-  vpc_id      = data.aws_vpc.default.id
-  
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_db_instance" "rds_db" {
   allocated_storage    = 15
   identifier           = "apple-store-db"
@@ -40,8 +17,7 @@ resource "aws_db_instance" "rds_db" {
   instance_class       =  var.instance_class
   username             = local.db_creds.DbUsername
   password             = local.db_creds.DbPassword
-
-  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
-  skip_final_snapshot  = true
-  publicly_accessible =  true
+  skip_final_snapshot = true
+  db_subnet_group_name = var.db_subnet_group_id
+  vpc_security_group_ids = [var.security_group_db_id]
 }
