160acd5f5f4ab1eae7cfd283c3e9abe3b27f617e
internshipdemoapp <147274235+internshipdemoapp@users.noreply.github.com>
2023-10-13 14:05:15 +0300

    Update modules/aws_ecr/main.tf file

--- modules/aws_ecr/main.tf
+++ modules/aws_ecr/main.tf
@@ -1,5 +1,9 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name = "app-repo"
-  force_delete = true
-  
+resource "aws_ecr_repository" "ecr" {
+  name         = "${lower(var.namespace)}/${var.service_name}"
+  force_delete = var.ecr_force_delete
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
+
