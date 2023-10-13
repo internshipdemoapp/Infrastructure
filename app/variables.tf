83ef0ff8d7ec89ef1076a46a35a7601c36c4e951
internshipdemoapp <147274235+internshipdemoapp@users.noreply.github.com>
2023-10-13 14:51:31 +0300

    Update app/variables.tf file

---
+++ app/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "namespace" {
+  type    = string
+  default = "dev"
+}
+
+variable "service_name" {
+  type    = string
+  default = "store-web-application"
+}
